284c9b75c3089eb20e1dbab7b0b3c3bee1c406c2
Camron Oakeson <Barking_Duuck@yahoo.com>
2019-08-26 17:12:53 -0600

    variables

---
+++ Git/Assets/Scripts/Assignments/variables.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class variables : MonoBehaviour
+{
+    string names = "Camron";
+
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(names);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
