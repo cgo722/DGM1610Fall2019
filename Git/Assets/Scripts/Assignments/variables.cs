bb2e89cd47f42681e328151e2090d3908491d77e
Camron Oakeson <Barking_Duuck@yahoo.com>
2019-08-28 17:05:49 -0600

    variables homework

--- Git/Assets/Scripts/Assignments/variables.cs
+++ Git/Assets/Scripts/Assignments/variables.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class variables : MonoBehaviour
 {
-    string names = "Camron";
+    string names = "Camron";                    //Strings are a string of letters
+    int age = 21;                               //integers are whole numbers
+    float height = 6.1f;                        //one must attach a f at the end of a float
+    bool married = false;                       //bools are true or false statements
 
-    
-    
-    
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(names);
+        Debug.Log("first name: " + names);      //When you string with a debug log you need to include the plus sign so it works
+        Debug.Log("Age: " + age);
+        Debug.Log("Height: " + height);
+        Debug.Log("Marital Status: " + married);
     }
 
     // Update is called once per frame
