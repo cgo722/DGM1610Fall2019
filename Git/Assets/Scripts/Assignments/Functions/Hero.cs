3ab40db02b5164ba10c30df7f427db22e22ac28b
Camron Oakeson <Barking_Duuck@yahoo.com>
2019-09-09 16:57:24 -0600

    Unity tutorial setup day

---
+++ Git/Assets/Scripts/Assignments/Functions/Hero.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+    string Superhero_name;
+    string Secret_identity;
+    int Age;
+
+    int num1;
+    int num2;
+    int total;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Superhero_name = "Spider-man";
+        Secret_identity = "Peter Parker";
+        Age = 16;
+        SHero(Superhero_name, Secret_identity, Age);
+        Calcsub(12, 10);
+    }
+
+    int Calcsub(int n1, int n2)
+    {
+        Debug.Log("Number 1 = " + n1 + " Number 2 = " + n2);
+        total = n1 - n2;
+
+        Debug.Log(total);
+        return total;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    
+    void SHero(string Sname, string Aname, int a)
+    {
+        Debug.Log(Sname + " " + Aname + " " + a);
+
+    }
+}
---
+++ Git/Assets/Scripts/Assignments/Functions/Villiany.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Villiany : MonoBehaviour
+{
+    string Villian_name;
+    string Species;
+    int age;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Villian_name = "Dirty Dan";
+        Species = "Fish";
+        age = 100000000;
+        Villian(Villian_name, Species, age);
+    }
+
+    int Calcm()
+    {
+
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void Villian(string Vname, string S, int A)
+    {
+        Debug.Log(Vname + " " + S + " " + A);
+    }
+}
