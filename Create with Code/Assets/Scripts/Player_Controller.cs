a686980923038f5d495ee1f68806badeddf25483
Camron Oakeson <Barking_Duuck@yahoo.com>
2019-09-11 17:09:01 -0600

    Lesson 1.3

--- Create with Code/Assets/Scripts/Player_Controller.cs
+++ Create with Code/Assets/Scripts/Player_Controller.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Player_Controller : MonoBehaviour
 {
+
+    private float speed = 30f; //adds a varible thingy in unity so we can change speed there
+    private float turnspeed;
+    private float horizontalInput;
+    private float forwardInput;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +19,10 @@ public class Player_Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal"); //Declares left and right button on keyboard as left and right turn
+        forwardInput = Input.GetAxis("Vertical"); //Delcares up as forward button
         //Move the Vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * 30);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput); //delta.Time is by units per sec instead of units per frame
+        transform.Rotate(Vector3.up, turnspeed * horizontalInput * Time.deltaTime);
     }
 }
