b7794d694fee9122480aee72729690824219c61e
Camron Oakeson <Barking_Duuck@yahoo.com>
2019-09-11 16:27:26 -0600

    Lesson 1.2

---
+++ Create with Code/Assets/Scripts/Player_Controller.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Move the Vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * 30);
+    }
+}
--- Git/Assets/Scripts/Assignments/Functions/Villiany.cs
+++ Git/Assets/Scripts/Assignments/Functions/Villiany.cs
@@ -8,6 +8,10 @@ public class Villiany : MonoBehaviour
     string Species;
     int age;
 
+    int num1;
+    int num2;
+    int total;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,12 +19,17 @@ public class Villiany : MonoBehaviour
         Species = "Fish";
         age = 100000000;
         Villian(Villian_name, Species, age);
+        Calcm(12, 1);
     }
 
-    int Calcm()
+    int Calcm(int n1, int n2)
     {
+        Debug.Log("Number 1 = " + n1 + " number 2 = " + n2);
+        total = n1 * n2;
 
-        
+        Debug.Log(total);
+        return total;
+
     }
     // Update is called once per frame
     void Update()
