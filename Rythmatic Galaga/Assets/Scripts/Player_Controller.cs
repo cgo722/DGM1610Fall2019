75985f31677454079dc4b6bb451566cee7e91cbc
Camron Oakeson <Barking_Duuck@yahoo.com>
2019-09-30 16:12:24 -0600

    Starting personal Project

---
+++ Rythmatic Galaga/Assets/Scripts/Player_Controller.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour
+{
+
+    public float vspeed;
+    public float hspeed;
+    public float horizontalInput;
+    public float verticalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector2.up * Time.deltaTime * vspeed * verticalInput);
+        transform.Translate(Vector2.right * Time.deltaTime * hspeed * horizontalInput);
+    }
+}
