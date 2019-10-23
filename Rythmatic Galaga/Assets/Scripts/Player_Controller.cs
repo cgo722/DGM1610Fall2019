7e2293d04dd73d81695b6c598fe2a223aad79b09
Camron Oakeson <Barking_Duuck@yahoo.com>
2019-10-23 17:03:27 -0600

    starting challenge 3 and working on personal project

--- Rythmatic Galaga/Assets/Scripts/Player_Controller.cs
+++ Rythmatic Galaga/Assets/Scripts/Player_Controller.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class Player_Controller : MonoBehaviour
 {
+
+    public float startDelay = 0;
+    public float frequencyOfShot = 0.2f;
     private float bulletRotation;
     public float vspeed;
     public float hspeed;
     public float horizontalInput;
     public float verticalInput;
     public GameObject projectilePrefab;
+    private Rigidbody2D rgb;
 
     // Start is called before the first frame update
     void Start()
     {
+        InvokeRepeating("ShootBullets", startDelay, frequencyOfShot);
+        rgb = GetComponent<Rigidbody2D>();
         
     }
 
@@ -23,6 +29,13 @@ public class Player_Controller : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Rotate(Vector3.forward * Time.deltaTime * hspeed * horizontalInput);
 
+
+
+
+    }
+
+    void ShootBullets()
+    {
         if (Input.GetKey(KeyCode.Space))
         {
             transform.Translate(Vector2.left * Time.deltaTime * vspeed);
@@ -31,6 +44,5 @@ public class Player_Controller : MonoBehaviour
             Instantiate(projectilePrefab, new Vector3(transform.position.x, transform.position.y, 0), transform.rotation);
 
         }
-        
     }
 }
