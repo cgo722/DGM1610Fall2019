f9cb2ad4a4b7c34198c334e5c24bfa8de8055edc
Camron Oakeson <Barking_Duuck@yahoo.com>
2019-11-13 11:11:52 -0700

    5.4 Completed

--- Create with Code Unit 5/Assets/Course Library/Scripts/GameManager.cs
+++ Create with Code Unit 5/Assets/Course Library/Scripts/GameManager.cs
@@ -14,13 +14,11 @@ public class GameManager : MonoBehaviour
     public TextMeshProUGUI gameOverText;
     public bool isGameActive = true;
     public Button restartButton;
+    public GameObject titleScreen;
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(SpawnTarget());
-        score = 0;
-        scoreText.text = "Score: " + score;
-        UpdateScore(0);
+
         
     }
 
@@ -51,5 +49,15 @@ public class GameManager : MonoBehaviour
     public void RestartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void StartGame(int difficulty)
+    {
+        spawnRate /= difficulty;
+        StartCoroutine(SpawnTarget());
+        score = 0;
+        scoreText.text = "Score: " + score;
+        UpdateScore(0);
+        titleScreen.gameObject.SetActive(false);
+
     }
 }
