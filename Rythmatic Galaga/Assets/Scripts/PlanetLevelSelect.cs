6d0f87260375d06de4695ac3dcc131e6dc098141
Camron Oakeson <Barking_Duuck@yahoo.com>
2020-02-13 17:22:23 -0700

    Started to Figure out sceneManager
    It is supposed to only activate when player is in contact with planet.

---
+++ Rythmatic Galaga/Assets/Scripts/PlanetLevelSelect.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlanetLevelSelect : MonoBehaviour
+{
+    public GameObject player;
+    public SceneManager scene;
+    public bool playerOnPlanet = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerOnPlanet == true && )
+        {
+
+        }
+    }
+}
